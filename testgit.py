7abe419bc4904f4d60bff3dcc2f7d21741e96bc3
Norilus <arnor21@ru.is>
2021-09-02 13:39:16 +0000

    smak my ass like the drum

--- testgit.py
+++ testgit.py
@@ -3,4 +3,10 @@ size= int(input("How large is the square?: "))
 # foreach row and column
 for row in range(size):
     for column in range(size):
-        print("*",end=" ")
+        if row == 0 or row == size - 1:
+        
+            print("*", end=" ")
+        
+        elif column == 0 or column == size:
+            prin("*", end=" ")
+    print()
